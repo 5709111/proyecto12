a83b03a41db1afff7492dddc3a8e986d9ff7f835
5709111 <5709111@alu.murciaeduca.es>
2022-09-23 21:19:48 +0200

    Segundo

--- app/libs/Application.php
+++ app/libs/Application.php
@@ -11,20 +11,50 @@
 
 class Application
 {
+    private $urlController = null;
+    private $urlAction = null;
+    private $urlParams = [];
+
     function __construct()
     {
-        print ('funciona¿');
+
         $db = Mysqldb::getInstance()->getDatabase();
 
         $url = $this->separarURL();
 
-        var_dump($url);
+        // para ver la url array var_dump($url);
+
+        //lo de abajo es para si no existe
+        if ( ! $this->urlController ){
+            require_once '../app/controllers/LoginController.php';
+            $page = new LoginController();
+            $page->index();
+        }elseif (file_exists('../app/controllers/' . ucfirst($this->urlController) . 'Controller.php'))
+        {
+            $controller = ucfirst($this->urlController) . 'Controller';
+            require_once '../app/controllers/' . $controller . '.php';
+            $this->urlController = new $controller;
+            $this->urlController->index();
+        }
 
     }
 
     public function separarURL()
     {
         if ($_SERVER['REQUEST_URI'] != '/'){
+            $url = trim($_SERVER['REQUEST_URI'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            //lo de abajo separa las cosas y los mete en un array
+            $url = explode('/', $url);
+
+            //lo de abajo es para que sea null si te lo mandan vacio en vez de dar error
+            $this->urlController = isset($url[0]) ?? null;
+            $this->urlAction = isset($url[1]) ?? null;
+
+            unset($url[0], $url[1]);
+
+            $this->urlParams = array_values($url);
 
         }
 
